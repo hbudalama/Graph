e126a6fca279a10ff7be7fa55f4db2083965e190
hbudalam <haneenbudalama@gmail.com>
2024-11-25 18:47:57 +0300

    added vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: '/', // Change this to '/repository-name/' if your app is hosted in a subdirectory
+  build: {
+    outDir: 'dist',
+  },
+});
