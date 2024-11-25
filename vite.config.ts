a8c679d91b31c3adc62566e1966668a0a1673b4c
hbudalam <haneenbudalama@gmail.com>
2024-11-25 19:15:17 +0300

    serving dist folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/', // Change this to '/repository-name/' if your app is hosted in a subdirectory
+  base: './', // Change this to '/repository-name/' if your app is hosted in a subdirectory
   build: {
     outDir: 'dist',
   },
